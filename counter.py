27875860d8cff9fa87ba4d03b676cb0eef7c9f32
Peter Dillow <peter.dillow@proton.me>
2022-11-24 09:11:55 -0800

    feat: added a get input function, and a multiprocessing statment

--- counter.py
+++ counter.py
@@ -1,10 +1,31 @@
-words = "Hi I am Peter"
+from multiprocessing import Pool
 
 def count_words(wordString):
     words = []
     for word in wordString.split():
         words.append(word)
     wordsLen = len(words)
-    print(wordsLen)
+    #print(wordsLen)
+    return wordsLen
 
-count_words(words)
+def get_input():
+    wordsString = input()
+    count_words(wordsString)
+
+#get_input()
+
+def_list = [
+    "Test 1",
+    "test 2",
+    "This is a test",
+    "The quick brown fox jumped over the lazy dog's back.",
+]
+
+if __name__ == '__main__':
+    for definition in def_list:
+        print("{} - {}".format(count_words(definition), definition))
+
+    with Pool(5) as p:
+        def_count = list(p.map(count_words, def_list))
+
+    print(dict(zip(def_count, def_list)))
