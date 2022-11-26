bc53cfd47d466f8e2f1ffb9e6dc1abda7569f4f4
Peter Dillow <peter.dillow@proton.me>
2022-11-25 18:37:00 -0800

    fix: removed multiprocessing from counter.py

--- counter.py
+++ counter.py
@@ -13,21 +13,3 @@ def get_input(priint):
     wordsNum = count_words(wordsString)
     if priint == True:
         print(wordsNum)
-
-#get_input()
-
-def_list = [
-    "Test 1",
-    "test 2",
-    "This is a test",
-    "The quick brown fox jumped over the lazy dog's back.",
-]
-
-if __name__ == '__main__':
-    for definition in def_list:
-        print("{} - {}".format(count_words(definition), definition))
-
-    with Pool(5) as p:
-        def_count = list(p.map(count_words, def_list))
-
-    print(dict(zip(def_count, def_list)))
